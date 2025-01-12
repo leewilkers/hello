feaa721607c867103edbb08e17678c3540b97e32
leewilkers <34852895+leewilkers@users.noreply.github.com>
2025-01-12 13:50:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// tippy('.interests-list li', {
+//   content(reference) {
+//     const id = reference.getAttribute('data-content-id');
+//     return document.getElementById(id)?.innerHTML || 'Content coming soon...';
+//   },
+//   allowHTML: true,
+//   delay: [100, 50],
+//   placement: 'right',
+//   theme: 'terminal',
+//   interactive: true
+// }); 
